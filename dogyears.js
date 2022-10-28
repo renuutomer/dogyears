fc5f84797587db38d32b69933fa5b02c7480054d
Renuu Tomer <108678640+renuutomer@users.noreply.github.com>
2022-10-28 18:45:24 +0100

    Create dogyears.js

---
+++ dogyears.js
@@ -0,0 +1,17 @@
+/ my current age
+const myAge = 30;
+//earlyyears
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName ="Renuu Tomer". toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
